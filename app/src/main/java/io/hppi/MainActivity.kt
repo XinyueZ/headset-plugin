a7c03483b9eef0e894d313a91a6a262fbb387c62
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-08-25 13:41:15 +0200

    ktlint code format

--- app/src/main/java/io/hppi/MainActivity.kt
+++ app/src/main/java/io/hppi/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.hppi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import io.hppi.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
