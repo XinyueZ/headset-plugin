f880cd22774c03f0f263d9c0209fc21b2e55dbde
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-09-01 15:35:47 +0200

    remove useless extenstion bind-adapter

--- app/src/main/java/io/hppi/adapters/BindingAdapters.kt
+++ app/src/main/java/io/hppi/adapters/BindingAdapters.kt
@@ -1,26 +1,7 @@
 package io.hppi.adapters
 
-import android.net.Uri
-import android.view.View
-import android.widget.MediaController
-import android.widget.VideoView
 import androidx.databinding.BindingAdapter
 import com.facebook.shimmer.ShimmerFrameLayout
-
-@BindingAdapter("src")
-fun VideoView.setSource(filename: String) {
-    val srcUri = Uri.parse("android.resource://${context.packageName}/raw/$filename")
-    setMediaController(MediaController(context).apply { this.visibility = View.GONE })
-    setVideoURI(Uri.parse(srcUri.toString()))
-    seekTo(0)
-    requestFocus()
-    setOnPreparedListener {
-        it.isLooping = true
-        it.setVolume(0f, 0f)
-        it.isLooping = true
-        start()
-    }
-}
 
 @BindingAdapter("startAnim")
 fun ShimmerFrameLayout.startAnim(start: Boolean) {
