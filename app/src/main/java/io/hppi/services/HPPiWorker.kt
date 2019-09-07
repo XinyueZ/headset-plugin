9222c587f3f7e24d3f13fa5bfb5a4ca18174763e
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-09-07 14:39:42 +0200

    resolved #14

--- app/src/main/java/io/hppi/services/HPPiWorker.kt
+++ app/src/main/java/io/hppi/services/HPPiWorker.kt
@@ -1,12 +1,16 @@
 package io.hppi.services
 
 import android.content.Context
-import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import io.hppi.repositories.HeadphoneStateRepository
+import java.util.concurrent.TimeUnit
+
+const val WORKER_REPEAT_MIN = 20L
+const val WORKER_REPEAT_FLEX = 6L
 
 class HPPiWorker(
     context: Context,
@@ -20,7 +24,12 @@ class HPPiWorker(
 }
 
 fun Context.enqueueHPPiWorker(): HeadphoneStateRepository {
-    val request: WorkRequest = OneTimeWorkRequest.Builder(HPPiWorker::class.java).build()
+    HeadphoneStateRepository.getInstance(applicationContext).setup(applicationContext)
+    val request: WorkRequest = PeriodicWorkRequest.Builder(
+        HPPiWorker::class.java,
+        WORKER_REPEAT_MIN, TimeUnit.MINUTES,
+        WORKER_REPEAT_FLEX, TimeUnit.MINUTES
+    ).build()
     WorkManager.getInstance(applicationContext).enqueue(request)
     return HeadphoneStateRepository.getInstance(applicationContext)
 }
